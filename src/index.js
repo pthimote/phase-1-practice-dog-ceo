6028b8b4bf1f52bb79cae9e9ede4b9ca3c021a50
Paul Thimote <pthimote@gmail.com>
2022-10-08 20:54:49 -0400

    commit tree c9dce403d955326d2f98a161e6894cc8a172738d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+const breeds = [] 
+document.addEventListener('DOMContentLoaded', function (){
+    loadImg()
+}) 
+
+function loadImg() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then((res) => res.json())
+    .then((results) => (results.message.forEach(img => addImg(img))))
+}
+function addImg(dogPic) {
+    const imgJs = document.getElementById('dog-image-container')
+    const pictures = document.createElement('img')
+    pictures.src = dogPic
+    imgJs.appendChild(pictures)
+}
